82b524293f99c472e68bd609a89339bd8dbc2a25
Андрей Передерий <anperq@mail.ru>
2025-10-13 15:27:53 +0300

    Automatic diff class Value works correctly

--- main.py
+++ main.py
@@ -1,3 +1,174 @@
 import torch
-print(torch.__version__)
-print(torch.cuda.is_available())
+
+class Value:
+    """ stores a single scalar value and its gradient """
+
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self.grad = 0
+        self._backward = lambda: None  # function to propagate gradients
+        self._prev = set(_children)  # set of parent Value nodes
+        self._op = _op  
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, _children=(self, other), _op='+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, _children=(self, other), _op='*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data ** other, _children=(self,), _op='**')
+
+        def _backward():
+            self.grad += (other * (self.data ** (other - 1))) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def relu(self):
+        out = Value(max(0, self.data), _children=(self,), _op='ReLU')
+
+        def _backward():
+            self.grad += (1.0 if self.data > 0 else 0.0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+
+        self.grad = 1.0
+        for v in reversed(topo):
+            v._backward()
+
+    def __neg__(self):  # -self
+        return self * -1
+
+    def __radd__(self, other):  
+        return self + other
+
+    def __sub__(self, other):  
+        return self + (-other)
+
+    def __rsub__(self, other):  
+        return other + (-self)
+
+    def __rmul__(self, other):  
+        return self * other
+
+    def __truediv__(self, other):  
+        return self * (other if isinstance(other, Value) else Value(other)) ** -1
+
+    def __rtruediv__(self, other):  
+        return (other if isinstance(other, Value) else Value(other)) * self ** -1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
+
+
+
+
+def test_sanity_check():
+    x = Value(-4.0)
+    z = 2 * x + 2 + x
+    q = z.relu() + z * x
+    h = (z * z).relu()
+    y = h + q + q * x
+    y.backward()
+    xmg, ymg = x, y
+
+    x = torch.Tensor([-4.0]).double()
+    x.requires_grad = True
+    z = 2 * x + 2 + x
+    q = z.relu() + z * x
+    h = (z * z).relu()
+    y = h + q + q * x
+    y.backward()
+    xpt, ypt = x, y
+
+    assert abs(ymg.data - ypt.data.item()) < 1e-6
+    print(xmg, xpt, xpt.grad)
+    assert abs(xmg.grad - xpt.grad.item()) < 1e-6
+
+
+def test_more_ops():
+    a = Value(-4.0)
+    b = Value(2.0)
+    c = a + b
+    d = a * b + b ** 3
+    c += c + 1
+    c += 1 + c + (-a)
+    d += d * 2 + (b + a).relu()
+    d += 3 * d + (b - a).relu()
+    e = c - d
+    f = e ** 2
+    g = f / 2.0
+    g += 10.0 / f
+    g.backward()
+    amg, bmg, gmg = a, b, g
+
+    a = torch.Tensor([-4.0]).double()
+    b = torch.Tensor([2.0]).double()
+    a.requires_grad = True
+    b.requires_grad = True
+    c = a + b
+    d = a * b + b ** 3
+    c = c + c + 1
+    c = c + 1 + c + (-a)
+    d = d + d * 2 + (b + a).relu()
+    d = d + 3 * d + (b - a).relu()
+    e = c - d
+    f = e ** 2
+    g = f / 2.0
+    g = g + 10.0 / f
+    g.backward()
+    apt, bpt, gpt = a, b, g
+
+    tol = 1e-6
+    assert abs(gmg.data - gpt.data.item()) < tol
+    assert abs(amg.grad - apt.grad.item()) < tol
+    assert abs(bmg.grad - bpt.grad.item()) < tol
+    print("test_more_ops passed correctly")
+
+
+
+if __name__ == "__main__":
+    a = Value(-4.0)
+    b = Value(2.0)
+    d = Value(3.0)
+
+    c = a + b
+    e = c * d
+    e.backward()
+
+    print(a, b, c, d, e)
+    test_sanity_check()
+    test_more_ops()
