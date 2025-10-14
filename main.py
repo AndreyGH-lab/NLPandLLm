d2c6e8e90430706a23c9f40add5071eb63b448ca
Андрей Передерий <anperq@mail.ru>
2025-10-14 11:30:45 +0300

    switch to branch development

--- main.py
+++ main.py
@@ -119,46 +119,6 @@ def test_sanity_check():
     assert abs(xmg.grad - xpt.grad.item()) < 1e-6
 
 
-def test_more_ops():
-    a = Value(-4.0)
-    b = Value(2.0)
-    c = a + b
-    d = a * b + b ** 3
-    c += c + 1
-    c += 1 + c + (-a)
-    d += d * 2 + (b + a).relu()
-    d += 3 * d + (b - a).relu()
-    e = c - d
-    f = e ** 2
-    g = f / 2.0
-    g += 10.0 / f
-    g.backward()
-    amg, bmg, gmg = a, b, g
-
-    a = torch.Tensor([-4.0]).double()
-    b = torch.Tensor([2.0]).double()
-    a.requires_grad = True
-    b.requires_grad = True
-    c = a + b
-    d = a * b + b ** 3
-    c = c + c + 1
-    c = c + 1 + c + (-a)
-    d = d + d * 2 + (b + a).relu()
-    d = d + 3 * d + (b - a).relu()
-    e = c - d
-    f = e ** 2
-    g = f / 2.0
-    g = g + 10.0 / f
-    g.backward()
-    apt, bpt, gpt = a, b, g
-
-    tol = 1e-6
-    assert abs(gmg.data - gpt.data.item()) < tol
-    assert abs(amg.grad - apt.grad.item()) < tol
-    assert abs(bmg.grad - bpt.grad.item()) < tol
-    print("test_more_ops passed correctly")
-
-
 
 if __name__ == "__main__":
     a = Value(-4.0)
@@ -169,6 +129,4 @@ if __name__ == "__main__":
     e = c * d
     e.backward()
 
-    #print(a, b, c, d, e)
     test_sanity_check()
-    test_more_ops()
